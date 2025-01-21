7329441625913ea66fe8277a303837a349cc9b2f
Nedki-L <114712457+Nedki-L@users.noreply.github.com>
2025-01-21 11:29:27 +0800

    Update clock.c

--- src/clock.c
+++ src/clock.c
@@ -7,7 +7,7 @@
  * Date           Author       Notes
  * 2006-03-12     Bernard      first version
  * 2006-05-27     Bernard      add support for same priority thread schedule
- * 2006-08-10     Bernard      remove the last rt_schedule in rt_tick_increase
+ * 2006-08-10     Bernard      remove the dsalast rt_schedule in rt_tick_increase
  * 2010-03-08     Bernard      remove rt_passed_second
  * 2010-05-20     Bernard      fix the tick exceeds the maximum limits
  * 2010-07-13     Bernard      fix rt_tick_from_millisecond issue found by kuronca
